91ea77ee866924ba25111530550f7d5af95ce4ce
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-09 23:54:24 +0530

    Lazy loading for Home and About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{lazy} from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+const Home=lazy(
+  ()=>{
+
+    return import("./pages/Home")
+  }
+)
+const About=lazy(()=>import("./pages/About"))
+
 
 const App = () => {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path='/' element={<h1>HOME</h1>}/>
-      <Route path='/about' element={<h1>ABOUT</h1>}/>
+      <Route path='/' element={<Home></Home>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path="/contact" element={<h1>CONTACT</h1>}/>
 
       
     </Routes>
