b4df6fc92785d76f03d2d010735fc9740167a1b7
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-09 23:39:21 +0530

    Initialised Client folder and installed some basic dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+const App = () => {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<h1>HOME</h1>}/>
+      <Route path='/about' element={<h1>ABOUT</h1>}/>
+
+      
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
